b6aba7d29f7cb6493472372bf50a7fd390dd42d0
Kate Moreland <kate.ella.moreland@gmail.com>
2025-09-18 23:21:49 -0500

    Update autotag-pdf.js

--- autotag-pdf.js
+++ autotag-pdf.js
@@ -49,7 +49,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: ['https://kmoreland126.github.io', '*'],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
   })
