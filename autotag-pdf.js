aa382cdc0913f398dce66e71f2457ef910eed850
Kate Moreland <kate.ella.moreland@gmail.com>
2025-09-19 23:56:20 -0500

    Updated autotag-pdf.js

--- autotag-pdf.js
+++ autotag-pdf.js
@@ -166,7 +166,7 @@ app.post('/upload-pdf', upload.single('file'), async (req, res) => {
     readStream?.destroy();
   }
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 // const express = require('express');
